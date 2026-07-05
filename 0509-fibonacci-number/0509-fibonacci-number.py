57b7b4adcdb027959f5a6a9acdf06a795b6241e0
Ankit Parmanik <ankitparmanik65@gmail.com>
2026-07-05 11:17:13 +0530

    Time: 325 ms (18.53%), Space: 19.3 MB (17.93%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        #Base case
+        if n==0 or n==1:
+            return n
+        
+        #Recursive case
+        return self.fib(n-1) + self.fib(n-2)
+        
